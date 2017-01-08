666dfa4867c17b769abc545affde5508f256a7d3
cptran777 <cptran14@gmail.com>
2017-01-08 11:33:31 -0800

    create new hide/show properties and delays to toggles to improve expand and shrink animation

--- app/components/index-tech.js
+++ app/components/index-tech.js
@@ -3,12 +3,28 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   expandSkills: false,
   expandProjects: false,
+  hideSkills: false,
+  hideProjects: false,
   actions: {
     toggleSkills() {
       this.toggleProperty('expandSkills');
+      if (this.get('expandSkills')) {
+        this.toggleProperty('hideProjects');
+      } else {
+        setTimeout(() => {
+          this.toggleProperty('hideProjects');
+        }, 1050);
+      }
     },
     toggleProjects() {
       this.toggleProperty('expandProjects');
+      if (this.get('expandProjects')) {
+        this.toggleProperty('hideSkills');
+      } else {
+        setTimeout(() => {
+          this.toggleProperty('hideSkills');
+        }, 1050);
+      }
     }
   }
 });
