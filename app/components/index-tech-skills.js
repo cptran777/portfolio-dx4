1303e2d08960aaa8066fa082d5a64603d8a3f598
cptran777 <cptran14@gmail.com>
2017-01-09 20:02:09 -0800

    create skills card for index page

--- app/components/index-tech-skills.js
+++ app/components/index-tech-skills.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
+const skillListA = ['js.png', 'html5.png', 'css3.png', 'jquery.gif', 'react.png', 'redux.png'];
+const skillListB = ['ember.png', 'angular.png', 'reactnative.png', 'node.png', 'express.png', 'websocket.png'];
+const skillListC = ['threejs.png', 'webrtc.png', 'd3.png', 'createjs.png', 'ajax.png', 'heroku.png'];
+const skillListD = ['mysql.png', 'redis.png', 'mongodb.png', 'postgres.png', 'bootstrap.png', 'c.png'];
+
+const concatAll = (str, arr) => {
+  return arr.map((item) => {
+    return str + item;
+  });
+};
+
 export default Ember.Component.extend({
+  skillLogoA: concatAll('assets/images/logo-', skillListA),
+  skillLogoB: concatAll('assets/images/logo-', skillListB),
+  skillLogoC: concatAll('assets/images/logo-', skillListC),
+  skillLogoD: concatAll('assets/images/logo-', skillListD)
 });
