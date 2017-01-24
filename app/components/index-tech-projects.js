caca50fd5321c809c141494cd571ea3939c8be45
cptran777 <cptran14@gmail.com>
2017-01-23 20:59:00 -0800

    add project images and dynamically render on page with some styling

--- app/components/index-tech-projects.js
+++ app/components/index-tech-projects.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
+const projectListMain = ['avalon.png', 'reeltime.png', 'labit.png'];
+const projectListOther = [];
+// const projectListProgress = [];
+
+const concatAll = (str, arr) => {
+  return arr.map((item) => {
+    return str + item;
+  });
+};
+
+const capitalize = (str) => {
+  return str[0].toUpperCase() + str.slice(1);
+};
+
 export default Ember.Component.extend({
+  mainProjects: concatAll('assets/images/project-', projectListMain).map((path, idx) => {
+    return {
+      name: capitalize(path.split(/[-.]/)[1]),
+      path
+    };
+  })
+
 });
